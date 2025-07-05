edfbf985099bc7d6e3b8592f21ae8dcf41118720
theripunbty <ripunbasumatary10@gmail.com>
2025-07-05 21:09:40 +0530

    Initial commit: Admin dashboard setup with ESLint disabled

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
